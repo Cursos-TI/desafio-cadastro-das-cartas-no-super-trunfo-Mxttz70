a2f83a3fd4ea3f8c92684b15b5f514a002966103
Mxttz70 <tkzxdz@gmail.com>
2025-03-21 15:44:45 +0000

    trabalho completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,7 +16,12 @@ int main(){
 
     struct cidade cidade1 = {'A', "A01", "São Paulo", 12325000, 1521.11, 699.28, 50};
     struct cidade cidade2 = {'B', "B02", "Rio de Janeiro", 6748000, 1200.25, 300.50, 30};
-     
+
+    float densidade1 = cidade1.populacao / cidade1.area;
+    float densidade2 = cidade2.populacao / cidade2.area;
+    float pib_per_capita1 = (cidade1.pib * 1000000000) / cidade1.populacao;
+    float pib_per_capita2 = (cidade2.pib * 1000000000) / cidade2.populacao;
+
     printf("Carta 1: \n");
     printf("Estado: %c\n", cidade1.estado);
     printf("Código: %s\n", cidade1.codigo);
@@ -24,7 +29,9 @@ int main(){
     printf("População: %i\n", cidade1.populacao);
     printf("Área: %.2f quilômetros quadrados \n", cidade1.area);
     printf("PIB: %.2f bilhões de reais \n", cidade1.pib);
-    printf("Número de pontos Turísticos: %d\n\n", cidade1.pontos_turisticos);
+    printf("Número de pontos Turísticos: %d\n", cidade1.pontos_turisticos);
+    printf("Densidade populacional: %.2f hanitantes por quilômetro quadrado\n", densidade1);
+    printf("PIB per capita: R$ %.2f\n\n", pib_per_capita1);
 
     printf("Carta 2: \n");
     printf("Estado: %c\n", cidade2.estado);
@@ -34,6 +41,9 @@ int main(){
     printf("Área: %.2f quilômetros quadrados \n", cidade2.area);
     printf("PIB: %.2f bilhões de reais \n", cidade2.pib);
     printf("Número de Pontos Turísticos: %d\n", cidade2.pontos_turisticos);
+    printf("Densidade populacional: %.2f habitentes por quilômetro quadrado\n", densidade2);
+    printf("PIB per capita: R$ %.2f\n", pib_per_capita2);
+    
 
     return 0;
 
